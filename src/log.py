2af62a7e9f0128c33807871c125c1f0b24fa57a6
letleon <letleon@163.com>
2024-12-06 22:07:23 +0800

    sync

--- src/log.py
+++ src/log.py
@@ -6,8 +6,8 @@ def setup_logger():
     """配置日志 - Configure logging"""
     logger = logging.getLogger("Addon Reloader")
     if not logger.hasHandlers():
-        logger.setLevel(logging.DEBUG)
-        # logger.setLevel(logging.INFO)
+        # logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.INFO)
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
         formatter = logging.Formatter("%(levelname)s:%(message)s")
